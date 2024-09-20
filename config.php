bb68f39debf50b752ba64eb5934cda96b2a0ebc1
Hoan Nguyen <167113079+HoaanDev@users.noreply.github.com>
2024-09-20 09:54:53 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME', 'id20032419_nhom3');
 define('DB_USER', 'id20032419_hoaan2204');
-define('DB_PASSWORD', 'HoanCoTam2204@');
+define('DB_PASSWORD', '*************@');
 define('DB_HOST', 'localhost');
-define('DB_PORT', 21);
+define('DB_PORT', 443);
 define('DB_CHARSET', 'utf8');
